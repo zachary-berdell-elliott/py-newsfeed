81cd9b6bdbc3b8d604ba53619354c241beae0f85
zachary <zacharyberdell@gmail.com>
2022-03-01 10:39:28 -0700

    Set up a validator for the password and added bcrypt so passwords are salted and hashed.

--- app/models/User.py
+++ app/models/User.py
@@ -2,6 +2,9 @@ from app.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import validates
 import re
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -14,3 +17,8 @@ class User(Base):
   def email_validator(self, key, email):
     emailInput = re.match(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email)
     return emailInput
+
+  @validates('password')
+  def password_validator(self, key, password):
+    assert len(password) > 8
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
