4d87838e69e04928c970fa65f53819c026141434
zachary <zacharyberdell@gmail.com>
2022-02-28 21:34:12 -0700

    Added env variable to __init__. Set up the home routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
+    SECRET_KEY=os.getenv('SECRET')
   )
 
   @app.route('/hello')
