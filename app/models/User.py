ff9e0820f7f7dbe362d18b40633893a32cc63ff7
zachary <zacharyberdell@gmail.com>
2022-03-01 10:33:18 -0700

    Added an email validator to the User model.

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,7 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import re
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +9,8 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('email')
+  def email_validator(self, key, email):
+    emailInput = re.match(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email)
+    return emailInput
