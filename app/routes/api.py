39cce0f1435a6b998795441a58b436daaee1f629
zachary <zacharyberdell@gmail.com>
2022-03-04 13:17:02 -0700

    Users are now added to the database after they sign up.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+  data = request.get_json()
+  db = get_db()
+  
+  #Creates a new user
+  newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+  )
+
+  # Saves user to the database
+  db.add(newUser)
+  db.commit()
+
+  return jsonify(id = newUser.id)
