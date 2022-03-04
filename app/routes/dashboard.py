df17400988480696e0f60f3b9aa2fc2e0c830efa
zachary <zacharyberdell@gmail.com>
2022-03-04 16:27:18 -0700

    Updated routes for the dashboard.

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+  posts = (
+    db.query(Post)
+    .filter(Post.user_id == session.get('user_id'))
+    .order_by(Post.created_at.desc())
+    .all()
+  )
+
+  return render_template(
+    'dashboard.html',
+    post=posts,
+    loggedIn=session.get('loggedIn')
+  )
 
 @bp.route('/edit/<id>')
 def edit(id):
