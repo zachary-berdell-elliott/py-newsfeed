ba08bfd971d4b39eb698e88676e1312276e895c1
zachary <zacharyberdell@gmail.com>
2022-03-04 17:02:31 -0700

    Set up a log in auth wrapper function.

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ import sys
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -60,6 +61,7 @@ def logout():
   return '', 204
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
   data = request.get_json()
   db = get_db()
@@ -83,6 +85,7 @@ def comment():
   return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
   data = request.get_json()
   db = get_db()
@@ -105,6 +108,7 @@ def upvote():
   return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
   data = request.get_json()
   db = get_db()
@@ -128,6 +132,7 @@ def create():
   return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
   data = request.get_json()
   db = get_db()
@@ -146,6 +151,7 @@ def update(id):
   return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
   db = get_db()
 
@@ -158,5 +164,5 @@ def delete(id):
 
     db.rollback()
     return jsonify(message = 'Post not found'), 404
-    
+
   return '', 204
